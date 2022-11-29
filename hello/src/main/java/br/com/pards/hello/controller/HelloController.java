01e2910a0bc4be4594e15ad486352d231a13cd81
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-29 12:25:59 -0300

    HelloController class and its code were created

--- hello/src/main/java/br/com/pards/hello/controller/HelloController.java
+++ hello/src/main/java/br/com/pards/hello/controller/HelloController.java
@@ -1,2 +1,14 @@
-package br.com.pards.hello.controller;public class HelloController {
+package br.com.pards.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //the annotation shows SB that this class serves URLs
+public class HelloController {
+
+    @GetMapping("/test") //on the url/test, it will return the result of running this method
+    public String sayHi(){
+
+        return "Hello world! My first SpringBoot project! This is very cool!!";
+    }
 }
