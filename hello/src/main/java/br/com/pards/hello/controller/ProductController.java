b3a79a5c81e2ff0f13444d059f71f9b3c2e1f92a
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-29 18:11:52 -0300

    Changes in ProductController class and its code

--- hello/src/main/java/br/com/pards/hello/controller/ProductController.java
+++ hello/src/main/java/br/com/pards/hello/controller/ProductController.java
@@ -1,2 +1,33 @@
-package br.com.pards.hello.controller;public class ProductController {
+package br.com.pards.hello.controller;
+
+import br.com.pards.hello.model.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    //Get ¬> used for data recovery
+    @GetMapping("/product") //all this structure is the endpoint
+    public Product showProduct(){
+        Product product = new Product();
+        product.setId(12345);
+        product.setName("Top-of-line computer");
+        product.setPrice(2500.00);
+        product.setQttStock(3);
+
+        return product;
+    }
+
+    //POST ¬> used to send data
+    @PostMapping("/product")
+    public String insertProduct(@RequestBody Product product){
+        System.out.println("Received: " + product.getId() + " - " + product.getName() + " - " + product.getPrice() +
+                " - " + product.getQttStock());
+        return ("Received product");
+    }
+
+
 }
