20ef6b41dfdaee9e71240ad55f454484921fdd04
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-29 12:20:14 -0300

    xml file responsible for telling the machine to download the library that will be used

---
+++ hello/src/main/java/br/com/pards/hello/HelloApplication.java
@@ -0,0 +1,14 @@
+package br.com.pards.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+}
