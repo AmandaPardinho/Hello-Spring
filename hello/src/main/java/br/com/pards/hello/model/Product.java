8162e4144014386af667aaf430ff2532fb381cd8
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-11-29 19:39:07 -0300

    Changes in Product class and its code

--- hello/src/main/java/br/com/pards/hello/model/Product.java
+++ hello/src/main/java/br/com/pards/hello/model/Product.java
@@ -1,2 +1,40 @@
-package br.com.pards.hello.model;public class Product {
+package br.com.pards.hello.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+    private int qttStock;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQttStock() {
+        return qttStock;
+    }
+
+    public void setQttStock(int qttStock) {
+        this.qttStock = qttStock;
+    }
 }
